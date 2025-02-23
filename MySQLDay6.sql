e821e4bf27ce47b289acf01a5cf113604f7b379f
taroshrao <taroshrao@gmail.com>
2025-02-23 23:55:23 +0530

    Create MySQLDay6.sql

---
+++ MySQLDay6.sql
@@ -0,0 +1,22 @@
+create database SkillCaptain;
+use SkillCaptain;
+CREATE TABLE Employees (
+    ID INT PRIMARY KEY,  
+    name VARCHAR(50),
+    email VARCHAR(255),
+    department VARCHAR(100),
+    dateofbirth DATE,
+    salary DECIMAL(15,2),
+    IsActive TINYINT DEFAULT 1 
+);
+
+insert into Employees
+VALUES
+(1, 'Tarosh Rao', 'taroshrao@gmail.com', 'Data Science', '1990-08-08', 1200000.00, 1),
+(2, 'Richa Bhardwaj', 'richa@gmail.com', 'HR', '1991-03-15', 2000000.50, 0);
+
+--drops the whole table from the database
+DROP TABLE Employees;
+
+--it only deletes the values of each cell but keeps the strivture of the table intact so that we can add other values inside it
+TRUNCATE TABLE Employees;
