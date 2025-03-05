c4f91c295d8dad5594d520f64be17fdd36bb1942
taroshrao <taroshrao@gmail.com>
2025-03-05 12:32:28 +0530

    Update MySQLDay8.sql

--- MySQLDay8.sql
+++ MySQLDay8.sql
@@ -16,7 +16,33 @@ from Employee e
 cross join Department d;
 
 -- 2. List of employees who belong to at least one department
+
 select distinct e.EmployeeName, d.DepartmentID
 from employee e
 inner join department d
 on e.DepartmentID = d.DepartmentID;
+
+--3. List of all employees and their department details, including employees without a department and departments without employees
+
+SELECT e.EmployeeID, e.EmployeeName, d.DepartmentID, d.DepartmentName
+FROM Employee e
+LEFT JOIN Department d ON e.DepartmentID = d.DepartmentID
+
+UNION
+
+SELECT e.EmployeeID, e.EmployeeName, d.DepartmentID, d.DepartmentName
+FROM Employee e
+RIGHT JOIN Department d ON e.DepartmentID = d.DepartmentID;
+
+--4. List of all employees, with department details if they belong to a department
+
+SELECT e.EmployeeID, e.EmployeeName, d.DepartmentID, d.DepartmentName
+FROM Employee e
+LEFT JOIN Department d ON e.DepartmentID = d.DepartmentID;
+
+--5. List of all departments, along with employee details if they have at least one employee
+
+SELECT e.EmployeeID, e.EmployeeName, d.DepartmentID, d.DepartmentName
+FROM Department d
+INNER JOIN Employee e ON e.DepartmentID = d.DepartmentID;
+
